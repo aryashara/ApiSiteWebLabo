21c54daadd68142e57e537c452eb3dbd5dba5de0
aryashara <tanyavhb@gmail.com>
2023-09-03 21:35:55 +0200

    Labo ApiSiteWeb Ajout Controller Utilisateur

--- ApiSiteWeb/Models/Utilisateur.cs
+++ ApiSiteWeb/Models/Utilisateur.cs
@@ -2,9 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ApiSiteWeb.DatabaseProjectApiSiteWeb;
+using Systeme.DatabaseProjectApiSiteWeb;
 
-namespace ApiSiteWeb.ApiSiteWeb.Models
+namespace ApiSiteWeb.Models
 {
     public class Utilisateur
     {
